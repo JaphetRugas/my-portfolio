d0ec6920728bab888493e9cc9e8f75f742e62922
Jade Japhet Rugas <rugas.jadejaphet.123@gmail.com>
2024-05-16 02:05:34 +0800

    Progress - Skills

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,6 +1,6 @@
 "use client"
-
-import { Chrome, Codepen, Database, Navigation, Type } from 'lucide-react'
+ 
+import Image from 'next/image'
 import React from 'react'
 
 export default function Skills() {
@@ -17,28 +17,72 @@ export default function Skills() {
             </div>
             <div className="grid grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
               <div className="flex flex-col items-center gap-2">
-                <Chrome className="h-12 w-12" />
+                <Image src={`/icons/javascript.png`} alt="icon" width="64" height="64" className="h-12 w-12" />
                 <span className="text-sm font-medium">JavaScript</span>
               </div>
               <div className="flex flex-col items-center gap-2">
-                <Type className="h-12 w-12" />
+                <Image src={`/icons/typescript.png`} alt="icon" width={100} height="64" className="h-12 w-12" />
                 <span className="text-sm font-medium">TypeScript</span>
               </div>
               <div className="flex flex-col items-center gap-2">
-                <Codepen className="h-12 w-12" />
+                <Image src={`/icons/nodejs.png`} alt="icon" width={100}  height={100} className="h-12 w-12" />
                 <span className="text-sm font-medium">Node.js</span>
               </div>
               <div className="flex flex-col items-center gap-2">
-                <Navigation className="h-12 w-12" />
+                <Image src={`/icons/javascript.png`} alt="icon" width="64" height="64" className="h-12 w-12" />
                 <span className="text-sm font-medium">Next.js</span>
               </div>
               <div className="flex flex-col items-center gap-2">
-                <Database className="h-12 w-12" />
+                <Image src={`/icons/mongo.png`} alt="icon" width="64" height="64" className="h-12 w-12" />
                 <span className="text-sm font-medium">MongoDB</span>
               </div>
               <div className="flex flex-col items-center gap-2">
-                <Database className="h-12 w-12" />
+                <Image src={`/icons/javascript.png`} alt="icon" width="64" height="64" className="h-12 w-12" />
                 <span className="text-sm font-medium">PostgreSQL</span>
+              </div>
+              <div className="flex flex-col items-center gap-2">
+                <Image src={`/icons/javascript.png`} alt="icon" width="64" height="64" className="h-12 w-12" />
+                <span className="text-sm font-medium">Zod</span>
+              </div>
+              <div className="flex flex-col items-center gap-2">
+                <Image src={`/icons/javascript.png`} alt="icon" width="64" height="64" className="h-12 w-12" />
+                <span className="text-sm font-medium">Express.js</span>
+              </div>
+              <div className="flex flex-col items-center gap-2">
+                <Image src={`/icons/javascript.png`} alt="icon" width="64" height="64" className="h-12 w-12" />
+                <span className="text-sm font-medium">Neon Console</span>
+              </div>
+              <div className="flex flex-col items-center gap-2">
+                <Image src={`/icons/javascript.png`} alt="icon" width="64" height="64" className="h-12 w-12" />
+                <span className="text-sm font-medium">HTML</span>
+              </div>
+              <div className="flex flex-col items-center gap-2">
+                <Image src={`/icons/javascript.png`} alt="icon" width="64" height="64" className="h-12 w-12" />
+                <span className="text-sm font-medium">CSS</span>
+              </div>
+              <div className="flex flex-col items-center gap-2">
+                <Image src={`/icons/javascript.png`} alt="icon" width="64" height="64" className="h-12 w-12" />
+                <span className="text-sm font-medium">Drizzle</span>
+              </div>
+              <div className="flex flex-col items-center gap-2">
+                <Image src={`/icons/javascript.png`} alt="icon" width="64" height="64" className="h-12 w-12" />
+                <span className="text-sm font-medium">Prisma</span>
+              </div>
+              <div className="flex flex-col items-center gap-2">
+                <Image src={`/icons/javascript.png`} alt="icon" width="64" height="64" className="h-12 w-12" />
+                <span className="text-sm font-medium">Postman</span>
+              </div>
+              <div className="flex flex-col items-center gap-2">
+                <Image src={`/icons/javascript.png`} alt="icon" width="64" height="64" className="h-12 w-12" />
+                <span className="text-sm font-medium">Zustand</span>
+              </div>
+              <div className="flex flex-col items-center gap-2">
+                <Image src={`/icons/javascript.png`} alt="icon" width="64" height="64" className="h-12 w-12" />
+                <span className="text-sm font-medium">Tailwind CSS</span>
+              </div>
+              <div className="flex flex-col items-center gap-2">
+                <Image src={`/icons/javascript.png`} alt="icon" width="64" height="64" className="h-12 w-12" />
+                <span className="text-sm font-medium">Cloudinary</span>
               </div>
             </div>
           </div>
